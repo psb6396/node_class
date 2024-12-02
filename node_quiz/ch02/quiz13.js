b70950d4e00645d7eb936e4ab2ee462aa54eb75b
psb6396 <psb6396@naver.com>
2024-12-02 15:12:13 +0900

    sd

--- node_quiz/ch02/quiz13.js
+++ node_quiz/ch02/quiz13.js
@@ -1,7 +1,14 @@
-const fs = require('fs')
+const fs = require('fs').promises
 
 function copyFile(src, dest) {
    //fs.copyFile을 사용하여 특정 파일(example.txt)을 새로운 파일(example-copy.txt)로 복사하는 프로그램을 작성
+   fs.copyFile(src, dest)
+      .then(() => {
+         console.log('복사완료')
+      })
+      .catch((error) => {
+         console.error(error)
+      })
 }
 
 copyFile('example.txt', 'example-copy.txt')
--- node_quiz/ch02/quiz14.js
+++ node_quiz/ch02/quiz14.js
@@ -1,3 +1,10 @@
 const EventEmitter = require('events')
+const fs = require('fs')
 
 const myEmitter = new EventEmitter()
+
+myEmitter.addListener('dataReceived', () => {
+   console.log('Hello, Node.js!')
+})
+
+// fs.watch('./quiz14.js')
