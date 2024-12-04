f9e37f5e10a7398ce61aa0074b9292e4413b332f
psb6396 <psb6396@naver.com>
2024-12-04 16:09:29 +0900

    d

--- node_quiz/ch03/quiz04.js
+++ node_quiz/ch03/quiz04.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 // 오류 처리 미들웨어
 app.use((err, req, res, next) => {
    console.error(err.stack)
-   res.status(___).send({ error: err.message })
+   res.status(err.status).send({ error: err.message })
 })
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'))
