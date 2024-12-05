f47187cdaaf551258a4045a66c4bbe733425474a
psb6396 <psb6396@naver.com>
2024-12-06 00:54:24 +0900

    d

--- node_quiz/ch03/quiz05.js
+++ node_quiz/ch03/quiz05.js
@@ -6,10 +6,16 @@ app.use(cookieParser())
 
 app.get('/setId', (req, res) => {
    // 1시간 동안 유효하고 name: id, value: user1인 쿠키를 만든다
+   res.cookie('name', 'id', {signed: false, maxAge: 1000 * 60 * 60})
+   res.cookie('value','user1',{signed: false, maxAge: 1000 * 60 * 60})
 })
 
 app.get('/getId', (req, res) => {
    // 쿠키 value값 읽어서 화면에 출력하기
+   console.log('cookies:', req.cookies)
+   res.send(`쿠키: ${req.cookies.age}`)
 })
+
+
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'))
