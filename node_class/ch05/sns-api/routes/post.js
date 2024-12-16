baeca3a1e8e015390f64343480adbcf73de29c87
psb6396 <psb6396@naver.com>
2024-12-16 16:18:37 +0900

    d

--- node_class/ch05/sns-api/routes/post.js
+++ node_class/ch05/sns-api/routes/post.js
@@ -74,9 +74,24 @@ router.post('/', isLoggedIn, upload.single('img'), async (req, res) => {
                 [HashTagInstace1, true] // #여행 해시태그가 새로 생성됨
                 [HashTagInstace2, true] // #맛집 해시태그가 새로 생성됨
             ]
+            
+            r[0] = HashTagInstace1
+            r[0] = HashTagInstace2
         */
+         // HashTagInstace를 통해 post와의 관계를 설정학고 이 과정에서 postHashtag 테이블의 postId와 hashtagId 컬럼에 값이 추가됨
+
          await post.addHashtags(result.map((r) => r[0]))
       }
+      res.json({
+         success: true,
+         post: {
+            id: post.id,
+            content: post.content,
+            img: post.img,
+            userId: post.userId,
+         },
+         message: '게시물이 성공적으로 등록되었습니다.',
+      })
    } catch (error) {
       console.error(error)
       res.status(500).json({ success: false, message: '게시물 등록 중 오류가 발생했습니다.' })
@@ -84,13 +99,82 @@ router.post('/', isLoggedIn, upload.single('img'), async (req, res) => {
 })
 
 //게시물 수정 localhost:8000/post/:id
-router.put('/:id', async (req, res) => {})
+router.put('/:id', isLoggedIn, async (req, res) => {
+   try {
+      //게시물 존재 여부 확인
+      // select * from post where id = ? and UserId = ?
+      const post = await Post.findOne({ where: { id: req.params.id, userId: req.user.id } })
+      if (!post) {
+         return res.status(404).json({ success: false, message: '게시물을 찾을 수 없습니다.' })
+      }
+
+      //게시물 수정
+      await post.update({
+         content: req.body.content,
+         img: req.file ? `/${req.file.filename}` : post.img, //수정된 이미지 파일이 있으면 교체 없으면 기존 값 유지
+      })
+      const hashtags = req.body.hashtags.match(/#[^\s#]*/g)
+      if (hashtags) {
+         const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({ where: { title: tag.slice(1) } })))
+
+         await post.addHashtags(result.map((r) => r[0]))
+      }
+
+      //업데이트 된 게시물 다시 조회
+      const updatedPost = await Post.findOne({
+         where: { id: req.params.id },
+         include: [
+            {
+               model: User,
+               attributes: ['id', 'nick'], //user테이블의 id,nick컬럼값만 가져옴
+            },
+            {
+               model: Hashtag,
+               attributes: ['title'], //hashtags 테이블의 title 컬럼 값만 가져옴
+            },
+         ],
+      })
+
+      res.json({
+         success: true,
+         post: updatedPost,
+         message: '게시물이 성공적으로 수정되었습니다.',
+      })
+   } catch (error) {
+      console.error(error)
+      res.status(500).json({ success: false, message: '게시물 수정 중 오류가 발생했습니다.' })
+   }
+})
 
 //게시물 삭제 localhost:8000/post/:id
-router.delete('/:id', async (req, res) => {})
+router.delete('/:id', isLoggedIn, async (req, res) => {
+   try {
+      // 삭제 게시물 존재 여부 확인
+      const post = await Post.findOne({ where: { id: req.params.id, UserId: req.user.id } })
+      if (!post) {
+         return res.status(404).json({ success: false, message: '게시물을 찾을 수 없습니다.' })
+      }
+      //게시물 삭제
+      await post.destroy()
+
+      res.json({
+         success: true,
+         message: '게시물이 성공적으로 삭제되었습니다.',
+      })
+   } catch (error) {
+      console.error(error)
+      res.status(500).json({ success: false, message: '게시물 삭제 중 오류가 발생했습니다.' })
+   }
+})
 
 //특정 게시물 불러오기(id로 게시물 조회) localhost:8000/post/:id
-router.get('/:id', async (req, res) => {})
+router.get('/:id', async (req, res) => {
+   try {
+   } catch (error) {
+      console.error(error)
+      res.status(500).json({ success: false, message: '게시물을 불러오는 중에 오류가 발생했습니다.' })
+   }
+})
 
 //전체 게시물 불러오기(페이징 기능) localhost:8000/post/
 router.get('/', async (req, res) => {})
